62471f2ef48f0d26f89f3f61aa118d9d5c54f38a
Pete Lowry <peter.m.lowry@gmail.com>
2025-01-22 10:12:41 +0000

    Add event listeners for static elements (to open/close modals and for New Game button)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log("Hello from script.js");
+// EVENT LISTENERS FOR STATIC ELEMENTS
+
+// Click button to show instructions (opens Instructions modal)
+document.getElementById("btn-show-instructions").addEventListener("click", () => {
+    document.getElementById("modal-instructions").classList.remove("hidden");
+});
+
+// Click close buttons (in all modal headers)
+const closeButtons = document.getElementsByClassName("btn-close-modal");
+for (btn of closeButtons) {
+    btn.addEventListener("click", (e) => {
+        let modalElement = e.target.closest(".modal-overlay");
+        modalElement.classList.add("hidden");
+    });
+};
+
+// Click "New Game" button
+document.getElementById("btn-new-game").addEventListener("click", newGame);
+
+// MAIN FUNCTION
+function newGame() {
+    console.log("New Game");
+}
