679b9b640f5714994074a0e043b412f60f418382
Pete Lowry <peter.m.lowry@gmail.com>
2025-01-22 10:59:09 +0000

    Add resetVisibility, getJumbleSwapNum and displayAlert functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,5 +19,50 @@ document.getElementById("btn-new-game").addEventListener("click", newGame);
 
 // MAIN FUNCTION
 function newGame() {
-    console.log("New Game");
+
+    // Reset styles (hidden class to hide/display elements)
+    resetVisibility();
+
+    // Game settings
+    const wordLength = parseInt(document.getElementById("grid-select").value);
+    const difficulty = document.getElementById("difficulty").value;
+    let remainingSwaps = 15;
+    let remainingBoxes = new Set();
+    let jumbleSwaps = getJumbleSwapNum(difficulty);
+    console.log(jumbleSwaps);
+
 }
+
+// HELPER FUNCTIONS
+function displayAlert(title, msg) {
+    document.getElementById("modal-alert-title").innerText = title;
+    document.getElementById("modal-alert-msg").innerHTML = `<p>${msg}</p>`;
+    document.getElementById("modal-alert").classList.remove("hidden");
+}
+
+function getJumbleSwapNum(difficulty) {
+    switch (difficulty.toLowerCase()) {
+        case "easy":
+            return 6;
+        case "medium":
+            return 8;
+        case "hard":
+            return 10;
+        default:
+            displayAlert("An Error Occurred", "Unrecognised difficulty rating")
+            throw new Error("Unrecognised difficulty rating");
+    }
+}
+
+function resetVisibility() {
+    // Hide elements
+    document.getElementById("game-end-info").classList.add("hidden");
+    document.getElementById("win").classList.add("hidden");
+    document.getElementById("lose").classList.add("hidden");
+    document.getElementById("definitions").classList.add("hidden");
+    document.getElementById("definitions").innerHTML = "";
+    // Show elements
+    document.getElementById("swaps-info").classList.remove("hidden");
+    document.getElementById("game-board").innerHTML = "<p>Generating board ...</p>";
+}
+
