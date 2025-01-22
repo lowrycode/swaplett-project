9d145818ea96d77becf2a82c19511c9f44648aff
Pete Lowry <peter.m.lowry@gmail.com>
2025-01-22 15:06:16 +0000

    Add functions for choosing the words which match the relevant criteria to be used in the grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,18 +31,72 @@ async function newGame() {
     let jumbleSwaps = getJumbleSwapNum(difficulty);
 
     // Get random words
+    let words = [];
     try {
-        const words = await fetchRandomWords(wordLength);
-        console.log(words);
+        words = await fetchRandomWords(wordLength);
     } catch (error) {
         displayAlert("Error", "Could not fetch words. Please check your internet connection.");
         console.error("FAILED TO FETCH RANDOM WORDS", error);
         // NOTE could use fallback words here e.g. return fallbackWords.filter(word => word.length === wordLength);
+        return;
     }
+    console.log(words);
+
+    // Get criteria for gridWords array
+    let criteria = [];
+    try {
+        criteria = getGridWordsCriteria(wordLength);
+    } catch (error) {
+        displayAlert("Error", "Invalid grid size");
+        console.error("FAILED TO GET CRITERIA FOR GRID WORDS", error);
+        return;
+    }
+
+    // Initialise gridWords array
+    let gridWords = []
+    try {
+        gridWords = initialiseGridWords(wordLength);
+    } catch (error) {
+        displayAlert("Error", "Invalid grid size");
+        console.error("FAILED TO INITIALISE GRID WORDS", error);
+        return;
+    }
+
+    // Build gridWords array
+    let getGridWords = assignGridWords(words, gridWords, criteria);
+    if (!getGridWords.success) {
+        displayAlert("Error", "Unable to build grid. To try again, press the New Game button");
+        console.error("FAILED TO ASSIGN GRID WORDS: could not fulfill the criteria");
+        return;
+    }
+    gridWords = getGridWords.data;
+    console.log(gridWords);
 
 }
 
 // HELPER FUNCTIONS
+
+function assignGridWords(words, gridWords, criteria, usedWords = new Set(), i = 0) {
+    if (i >= gridWords.length) return {data: gridWords, success: true};  // gridWords complete
+    for (let word of words) {
+        if (usedWords.has(word)) continue;
+        // Add word
+        gridWords[i] = word;
+        usedWords.add(word);
+        if (matchesCriteria(i, gridWords, criteria)) {
+            // Recursive call
+            // console.log(word, "passed tests - call again", gridWords);
+            const response = assignGridWords(words, gridWords, criteria, usedWords, i + 1);
+            if (response.success) return response;
+        }
+        // Backtrack
+        gridWords[i] = null;
+        usedWords.delete(word);
+        // console.log(word, "failed tests - try next word", gridWords);
+    }
+    return {data: gridWords, success: false};
+}
+
 function displayAlert(title, msg) {
     document.getElementById("modal-alert-title").innerText = title;
     document.getElementById("modal-alert-msg").innerHTML = `<p>${msg}</p>`;
@@ -81,6 +135,55 @@ async function fetchRandomWords(wordLength) {
     }
 }
 
+/**
+ * Returns information about where the common letters are within the grid words.
+ * This is used when building the "gridWords" array.
+ * 
+ * @param {number} wordLength The length of the words used in the gridWords array
+ * @returns {number[][]} A two-dimensional array where each sub-array contains four numbers:
+ *                       [gridWord index for word 1, gridWord index for word 2, character index for word 1, character index for word 2].
+ * @throws {Error} If the wordLength is invalid.
+ */
+function getGridWordsCriteria(wordLength) {
+    switch (wordLength) {
+        case 3:
+        case 4:
+            return [
+                [0, 2, 0, 0],
+                [0, 3, 2, 0],
+                [1, 2, 0, 2],
+                [1, 3, 2, 2]
+            ]
+        case 5:
+        case 6:
+            return [
+                [0, 3, 0, 0],
+                [0, 4, 2, 0],
+                [0, 5, 4, 0],
+                [1, 3, 0, 2],
+                [1, 4, 2, 2],
+                [1, 5, 4, 2],
+                [2, 3, 0, 4],
+                [2, 4, 2, 4],
+                [2, 5, 4, 4]
+            ]
+        case 7:
+            return [
+                [0, 3, 0, 0],
+                [0, 4, 3, 0],
+                [0, 5, 6, 0],
+                [1, 3, 0, 3],
+                [1, 4, 3, 3],
+                [1, 5, 6, 3],
+                [2, 3, 0, 6],
+                [2, 4, 3, 6],
+                [2, 5, 6, 6]
+            ]
+        default:
+            throw new Error("Invalid wordLength passed as argument");
+    }
+}
+
 function getJumbleSwapNum(difficulty) {
     switch (difficulty.toLowerCase()) {
         case "easy":
@@ -93,6 +196,33 @@ function getJumbleSwapNum(difficulty) {
             displayAlert("An Error Occurred", "Unrecognised difficulty rating");
             throw new Error("Unrecognised difficulty rating");
     }
+}
+
+function initialiseGridWords(wordLength) {
+    switch (wordLength) {
+        case 3:
+        case 4:
+            return Array(4).fill(null);
+        case 5:
+        case 6:
+        case 7:
+            return Array(6).fill(null);
+        default:
+            throw new Error("Unrecognised wordLength passed as argument");
+    }
+}
+
+function matchesCriteria(lastIndex, gridWords, criteria) {
+    for (let [iGridWord, jGridWord, iChar, jChar] of criteria) {
+        if (((iGridWord === lastIndex || jGridWord === lastIndex))
+            && gridWords[iGridWord]
+            && gridWords[jGridWord]
+            && (gridWords[iGridWord][iChar] !== gridWords[jGridWord][jChar])
+        ) {
+            return false;
+        }
+    }
+    return true;
 }
 
 function resetVisibility() {
